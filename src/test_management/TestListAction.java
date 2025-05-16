0249629879df0fb0bfcc7ff59bbad0aa4f4de3ee
k_yagi <k_yagi@LAPTOP-A26D0H8V>
2025-05-16 12:01:38 +0900

    a

--- src/test_management/TestListAction.java
+++ src/test_management/TestListAction.java
@@ -1,34 +1,83 @@
 package test_management;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import bean.Student;
-import dao.StudentDao;
+import bean.School;
+import bean.Subject;
+import bean.Teacher;
+import dao.ClassNumDao;
+import dao.SubjectDao;
+import tool.Action;
 
-@WebServlet(urlPatterns = {"/testmanagement/list"})
-public class TestListAction extends HttpServlet {
+public class TestListAction implements Action {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	PrintWriter out = response.getWriter();
+	@Override
 
-        try {
-            StudentDao dao = new StudentDao();
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-			int entYear = 0;
-			List<Student> student_ent = dao.filter(entYear);
+		// TODO 自動生成されたメソッド・スタブ
 
-            request.setAttribute("student_ent", student_ent);
-            request.getRequestDispatcher("/test_management/test_list.jsp").forward(request, response);
-        } catch (Exception e) {
-        	request.getRequestDispatcher("/main/error.jsp").forward(request, response);
-        }
-    }
+		//ローカル変数の宣言1
+
+		HttpSession session = req.getSession();//セッション
+
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		ClassNumDao cNumDao = new ClassNumDao(); // クラス番号Dao
+
+		SubjectDao subjectDao = new SubjectDao(); //科目Dao
+
+		School teacherSchool = teacher.getSchool(); // テスト情報
+
+		LocalDate todaysDate = LocalDate.now(); // LocalDateインスタンスを取得
+
+		int year = todaysDate.getYear(); // 現在の年を取得
+
+		//DBからデータ取得3
+
+		List<String>cNumlist = cNumDao.filter(teacherSchool); //クラス情報
+
+		List<Subject>list = subjectDao.filter(teacherSchool); //科目情報
+
+		//ビジネスロジック4
+
+		// リストを初期化
+
+		List<Integer> entYearSet = new ArrayList<>();
+
+		// 10年前から10年後まで年をリストに追加
+
+		for (int i = year - 10; i < year + 11; i++) {
+
+			entYearSet.add(i);
+
+		}
+
+		//レスポンス値をセット6
+
+		//リクエストにクラス情報リストをセット
+
+		req.setAttribute("cNumlist", cNumlist);
+
+		//リクエストに科目情報リストをセット
+
+		req.setAttribute("list", list);
+
+		//リクエストに入学年度リストをセット
+
+		req.setAttribute("entYearSet", entYearSet);
+
+		//jspへのフォワード7
+
+		req.getRequestDispatcher("test_list.jsp").forward(req , res);
+
+	}
+
 }
+
