f66158fbe9fe07b02c3ccce36ae047e937735e54
k_yagi <k_yagi@LAPTOP-A26D0H8V>
2025-05-16 12:08:50 +0900

    a

--- src/tool/Action.java
+++ src/tool/Action.java
@@ -3,6 +3,8 @@ package tool;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Action {
-    void execute(HttpServletRequest req, HttpServletResponse res) throws Exception;
+public abstract class Action {
+    public abstract void execute(
+    		HttpServletRequest request, HttpServletResponse response
+    		) throws Exception;
 }
