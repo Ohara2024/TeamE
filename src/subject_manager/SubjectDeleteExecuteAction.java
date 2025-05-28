9135c7549a1e2e140096da577f6ac483b6870613
r_matubara <r_matubara@LAPTOP-8DMLBLAE>
2025-05-28 12:19:42 +0900

    a

--- src/subject_manager/SubjectDeleteExecuteAction.java
+++ src/subject_manager/SubjectDeleteExecuteAction.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import bean.Subject;
 import dao.SubjectDao;
 
-@WebServlet("/subjectmanager/SubjectDeleteExecuteAction")
 public class SubjectDeleteExecuteAction extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
