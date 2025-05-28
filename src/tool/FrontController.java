b28132edf58b4343a65e4a49d241db10c928288d
admin <admin@LAPTOP-FRBSDC3F>
2025-05-28 12:16:31 +0900

    test

--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -3,24 +3,39 @@ package tool;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = { "*.action" })
 public class FrontController extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        execute(req, res);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		try {
+			// パスを取得
+			String path = req.getServletPath().substring(1);
+			// ファイル名を取得しクラス名に変換
+			String name = path.replace(".a", "A").replace('/', '.');
+			// アクションクラスのインスタンスを返却
+			Action action = (Action) Class.forName(name).getDeclaredConstructor().newInstance();
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        execute(req, res);
-    }
+			// 遷移先URLを取得
+			action.execute(req, res);
 
-    private void execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
-        req.getRequestDispatcher("/index.jsp").forward(req, res);
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+			// エラーページへリダイレクト
+			req.getRequestDispatcher("/error.jsp").forward(req, res);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		doGet(req,res);
+
+	}
 }
